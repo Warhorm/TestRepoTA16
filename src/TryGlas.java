0653cbe4938b814730864b208e8662cc6a84e414
Warhorm <aindros@gmail.com>
2017-09-20 11:35:11 +0300

    Lisain Error Kommetaari

--- src/TryGlas.java
+++ src/TryGlas.java
@@ -4,6 +4,7 @@ public class TryGlas {
 	public static void main(String[] args) {
 		// Janar aitab mind hetkel
 		System.out.println("Say Hello");
+		System.err.println("Kood ei ole Indrekule");
 
 	}
 
